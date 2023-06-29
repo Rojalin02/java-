f1ee723465ddcf86fc25b05f75fd97019b69a445
Rojalin <113226743+Rojalin02@users.noreply.github.com>
2023-06-29 18:13:52 +0530

    Add files via upload

---
+++ lit.java
@@ -0,0 +1,15 @@
+public class lit{
+    public static void main(String[] args) {
+        byte Age = 29; 
+        int age =30;
+        short aGe = 32;
+        long agE = 4521236987554265L;
+        char ch ='A';
+        float f=2.5f;
+        double d=2.5;
+        boolean b=true;
+        System.out.println(f);
+        String str = "finished";
+        System.out.println(str);
+    }
+    }  
