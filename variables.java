a472136e604db093a65a5b409e7884019c59f93a
Rojalin <113226743+Rojalin02@users.noreply.github.com>
2023-06-29 18:13:26 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String a[])
+    {
+        int num1 =453;
+        int num2 = 545;
+        int result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
