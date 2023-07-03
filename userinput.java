b74a6e8d4bd826e955faf11e8ef7744b100f1b00
Rojalin <113226743+Rojalin02@users.noreply.github.com>
2023-07-03 19:04:09 +0530

    userinput.java

--- userinput.java
+++ userinput.java
@@ -1,15 +1,16 @@
-    import java.util.Scanner;
-    public class userinput {
-    public static void main(String args[])
+   import java.util.Scanner;
+public class userinput
+{
+    public static void main(String[] args)
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number 1: ");
-        int a = sc.nextInt();
-        System.out.println("enter number 2: ");
-        int b= sc.nextInt();
-        int sum= a+b;
-        System.out.println("the sum is " +sum);
-    
+         Scanner sc = new Scanner(System.in); 
+            System.out.println("Enter the number");
+            int a = sc.nextInt();
+            System.out.println("enter the second number: ");
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println("The sum of two numbers is "+sum);     
     }
 }
+
 
