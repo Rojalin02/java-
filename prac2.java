d7c0cd3d2fb5ace2608149c2a2c88fd298476a44
Rojalin <113226743+Rojalin02@users.noreply.github.com>
2023-07-03 22:54:24 +0530

    Add files via upload

---
+++ prac2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class prac2
+ {
+    public static void main(String args[])
+    {
+      Scanner cs = new Scanner(System.in);
+      System.out.println("enter your name: ");
+      String str = cs.nextLine();
+      System.out.print("hello " +str);
+      System.out.print( ", have a good day");
+
+    }
+    
+}
