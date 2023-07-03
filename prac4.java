26fc17b2266e075c8423e5730c893c153346c2fe
Rojalin <113226743+Rojalin02@users.noreply.github.com>
2023-07-04 01:33:08 +0530

    Add files via upload

---
+++ prac4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class prac4 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter an integer: ");
+        System.out.println(sc.hasNextInt());
+    }
+    
+}
