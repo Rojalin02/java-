10283a78cfd61b20ca7aa62a4666b0fe069109e9
Rojalin <113226743+Rojalin02@users.noreply.github.com>
2023-07-05 11:10:24 +0530

    Add files via upload

---
+++ stringss.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class stringss{
+    public static void main(String args[]){
+        String name = "rosalin";
+        System.out.print("the name is: " +name);
+         
+        int a = 63;
+        float b = 3.4759f;
+        System .out .format("\n value of a is %d and value of b is %f",a ,b);
+       
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
